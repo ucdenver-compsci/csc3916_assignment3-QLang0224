f6c290042a0a0f311c0648b294bb5c0d7efabeee
QLang0224 <157908068+QLang0224@users.noreply.github.com>
2024-03-29 12:02:41 -0600

    Update Movies.js

--- Movies.js
+++ Movies.js
@@ -5,7 +5,6 @@ mongoose.connect(process.env.DB);
 
 // Movie schema
 var MovieSchema = new Schema({
-const MovieSchema = new mongoose.Schema({
   title: { type: String, required: true, index: true },
   releaseDate: { type: Number, min: [1900, 'Must be greater than 1899'], max: [2100, 'Must be less than 2100']},
   genre: {
